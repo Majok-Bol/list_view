208110e9291431339d86cc2fefc40cc3088b008a
Majok-Bol <bolmajok001@gmail.com>
2025-04-01 19:01:58 +0300

    added icons to the image container

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,24 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        // backgroundColor: Colors.blue,
         appBar: AppBar(title: Text('List View demo',style: TextStyle(color: Colors.white,fontSize: 25),),centerTitle: true,backgroundColor: Colors.purple,),
         body: Container(
           padding: EdgeInsets.all(10),
           margin: EdgeInsets.all(10),
-          color: Colors.grey,
+            child:(
+              ListView(children:const<Widget> [
+                ListTile(leading:Icon(Icons.map),title: Text('Map'),),
+                ListTile(leading:Icon(Icons.settings),title: Text('Settings'),),
+                ListTile(leading:Icon(Icons.storage),title: Text('Storage'),),
+                ListTile(leading:Icon(Icons.call),title: Text('Contact'),),
+                ListTile(leading:Icon(Icons.photo_album),title: Text('Album'),),
+                ListTile(leading:Icon(Icons.camera),title: Text('Camera'),),
+                ListTile(leading:Icon(Icons.edit),title: Text('Edit'),),
+              ],)
+            ),
+          ),
         ),
-      ),
     );
   }
 }
